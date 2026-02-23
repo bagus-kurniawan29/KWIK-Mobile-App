b6161afd168df9ea09a86e3f1691319df4828b75
bagus-kurniawan29 <contdbk29@gmail.com>
2026-02-23 10:00:00 +0800

    Nav bar

--- lib/main_navigation.dart
+++ lib/main_navigation.dart
@@ -25,7 +25,18 @@ class _MainNavigationState extends State<MainNavigation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: _screen[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(items: 
+        const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_max_outlined)),
+          BottomNavigationBarItem(icon: Icon(Icons.search_outlined)),
+          BottomNavigationBarItem(icon: Icon(Icons.mail_lock_outlined)),
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline)),
+        ],
+        type: BottomNavigationBarType.shifting,
+        selectedIconTheme: IconThemeData(fill: 2.5),
+        unselectedItemColor: Colors.grey,
+      ),
     );
   }
 }
