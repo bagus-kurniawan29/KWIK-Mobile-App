fbfd54af32f6a8a396f7e36884729b6fb164ca2e
bagus-kurniawan29 <contdbk29@gmail.com>
2026-02-25 12:00:00 +0800

    Third Commit

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -7,7 +7,7 @@ class Homepage extends StatefulWidget {
   @override
   State<Homepage> createState() => _HomepageState();
 }
-
+  
 class _HomepageState extends State<Homepage> {
   int i = 0;
   bool isLike = false;
@@ -57,145 +57,133 @@ class _HomepageState extends State<Homepage> {
         body: Stack(
           children: [
             SizedBox.expand(
-              child: Stack(
-                children: [
-                  SingleChildScrollView(
-                    physics: BouncingScrollPhysics(
-                      decelerationRate: ScrollDecelerationRate.normal,
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  CircleAvatar(
-                                    backgroundColor: Colors.black,
-                                    backgroundImage: AssetImage(
-                                      'images/pp.png',
-                                    ),
-                                  ),
-                                  SizedBox(width: 10),
-                                  Text(
-                                    'John Doe',
-                                    style: TextStyle(
-                                      fontFamily: 'sfprobold',
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                  SizedBox(width: 10),
-                                  Text(
-                                    '@johndoe',
-                                    style: TextStyle(
-                                      fontFamily: 'sfpro',
-                                      color: Colors.grey[500],
-                                    ),
-                                  ),
-                                ],
+            child: Stack(
+            children: [
+            SingleChildScrollView(
+              physics: BouncingScrollPhysics(
+                decelerationRate: ScrollDecelerationRate.normal,
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.black,
+                              backgroundImage: AssetImage('images/pp.png'),
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              'John Doe',
+                              style: TextStyle(
+                                fontFamily: 'sfprobold',
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
                               ),
-                              ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.black,
-                                  foregroundColor: Colors.white,
-                                ),
-                                onPressed: () {},
-                                child: Text('Follow'),
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              '@johndoe',
+                              style: TextStyle(
+                                fontFamily: 'sfpro',
+                                color: Colors.grey[500],
                               ),
-                            ],
+                            ),
+                          ],
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            foregroundColor: Colors.white,
                           ),
-                          SizedBox(height: 12),
-                          Text(
-                            '''"Sejauh mata memandang, hanya ada hamparan hijau dan ketenangan. Di antara lekuk jalan dan megahnya gunung, tersimpan cerita yang tak lekang oleh waktu. 🌾⛰️
+                          onPressed: () {},
+                          child: Text('Follow'),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 12,),
+                    Text('''"Sejauh mata memandang, hanya ada hamparan hijau dan ketenangan. Di antara lekuk jalan dan megahnya gunung, tersimpan cerita yang tak lekang oleh waktu. 🌾⛰️
 Inilah definisi nyata dari sebuah 'Tanah Legenda'. Tempat di mana waktu seolah berhenti sejenak."
 #TanahLegenda #IndonesianScenery #ExploreNature #Ketenangan #LandscapePhotography #VibesPedesaan''',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.black87,
-                              height: 1.5,
-                              fontFamily: 'sfpro',
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 4,
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(top: 20),
-                            height: 200,
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage('images/placeholder.png'),
-                                fit: BoxFit.cover,
-                              ),
-                              color: Colors.grey,
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  IconButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        isLike = !isLike;
-                                        if (isLike) {
-                                          i++;
-                                        } else {
-                                          i--;
-                                        }
-                                      });
-                                    },
-                                    icon: FaIcon(
-                                      isLike
-                                          ? FontAwesomeIcons.solidHeart
-                                          : FontAwesomeIcons.heart,
-                                      color: isLike ? Colors.red : Colors.grey,
-                                    ),
-                                  ),
-                                  Text(
-                                    '$i K',
-                                    style: TextStyle(fontFamily: 'sfpro'),
-                                  ),
-                                ],
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                icon: FaIcon(FontAwesomeIcons.comment),
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                icon: FaIcon(FontAwesomeIcons.paperPlane),
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                icon: FaIcon(FontAwesomeIcons.ellipsis),
-                              ),
-                            ],
-                          ),
-                          Divider(thickness: 1),
-                        ],
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black87,
+                        height: 1.5,
+                        fontFamily: 'sfpro'
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 4,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 20),
+                      height: 200,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('images/placeholder.png'),
+                          fit: BoxFit.cover,
+                        ),
+                        color: Colors.grey,
                       ),
                     ),
-                  ),
-                  Positioned(
-                    bottom: 15,
-                    right: 12,
-                    child: FloatingActionButton(
-                      onPressed: () {},
-                      shape: CircleBorder(),
-                      backgroundColor: Colors.black,
-                      child: Icon(Icons.add, color: Colors.white),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isLike = !isLike;
+                              if (isLike){
+                                i++;
+                              } else {
+                                i--;
+                              }
+                            });
+                          },
+                          icon: FaIcon(isLike ? FontAwesomeIcons.solidHeart : FontAwesomeIcons.heart ,color: isLike? Colors.red : Colors.grey, ),
+                        ),
+                        Text('$i K', style: TextStyle(fontFamily: 'sfpro'),),
+                          ],
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: FaIcon(FontAwesomeIcons.comment),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: FaIcon(FontAwesomeIcons.paperPlane),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: FaIcon(FontAwesomeIcons.ellipsis),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
+                    Divider(
+                      thickness: 1,
+                    )
+                  ],
+                ),
               ),
             ),
-          ],
+            Positioned(
+              bottom: 15,
+              right: 12,
+              child: FloatingActionButton(
+                onPressed: () {},
+                shape: CircleBorder(),
+                backgroundColor: Colors.black,
+                child: Icon(Icons.add, color: Colors.white),
+              ),
+            ),
+        ]))],
         ),
       ),
     );
