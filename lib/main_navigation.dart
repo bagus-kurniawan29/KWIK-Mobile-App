12a05c1e45c80d8b0588285a837f7797ff7bf6d4
bagus-kurniawan29 <contdbk29@gmail.com>
2026-02-25 22:36:18 +0800

    second commit

--- lib/main_navigation.dart
+++ lib/main_navigation.dart
@@ -4,7 +4,6 @@ import 'package:kwik/screens/mail.dart';
 import 'package:kwik/screens/profile.dart';
 import 'package:kwik/screens/search.dart';
 
-
 class MainNavigation extends StatefulWidget {
   const MainNavigation({super.key});
 
@@ -19,7 +18,7 @@ class _MainNavigationState extends State<MainNavigation> {
     const Homepage(),
     const Search(),
     const Mail(),
-    const Profile()
+    const Profile(),
   ];
 
   @override
@@ -27,34 +26,34 @@ class _MainNavigationState extends State<MainNavigation> {
     return Scaffold(
       body: _screen[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        items: [ BottomNavigationBarItem(
-          icon: _currentIndex == 0 ? 
-          const Icon(Icons.home) :
-          const Icon(Icons.home_outlined),
-          label: ""
-        ),
-         BottomNavigationBarItem(
-          icon: _currentIndex == 1 ? 
-          const Icon(Icons.search) :
-          const Icon(Icons.search_outlined),
-          label: ""
-        ),
-         BottomNavigationBarItem(
-          icon: _currentIndex == 2 ? 
-          const Icon(Icons.mail) :
-          const Icon(Icons.mail_outlined),
-          label: ""
-
-        ),
-         BottomNavigationBarItem(
-          icon: _currentIndex == 3 ? 
-          const Icon(Icons.person) :
-          const Icon(Icons.person_outline),
-          label: "",
-        ),
+        items: [
+          BottomNavigationBarItem(
+            icon: _currentIndex == 0
+                ? const Icon(Icons.home)
+                : const Icon(Icons.home_outlined),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: _currentIndex == 1
+                ? const Icon(Icons.search)
+                : const Icon(Icons.search_outlined),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: _currentIndex == 2
+                ? const Icon(Icons.mail)
+                : const Icon(Icons.mail_outlined),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: _currentIndex == 3
+                ? const Icon(Icons.person)
+                : const Icon(Icons.person_outline),
+            label: "",
+          ),
         ],
         currentIndex: _currentIndex,
-        onTap: (index){
+        onTap: (index) {
           setState(() {
             _currentIndex = index;
           });
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -6,6 +6,7 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         actionsPadding: EdgeInsets.only(right: 12),
         title: Text(
@@ -16,11 +17,77 @@ class Homepage extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+        backgroundColor: Colors.white,
         actions: [
           IconButton(onPressed: () {}, icon: Icon(Icons.notifications_rounded)),
         ],
       ),
-      body: Padding(padding: EdgeInsets.all(16.0)),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            physics: BouncingScrollPhysics(
+              decelerationRate: ScrollDecelerationRate.normal
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'Feed',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: "sfpro",
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        'Comunity',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: "sfpro",
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Text(
+                        'Friend',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: "sfpro",
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  ...List.generate(
+                    20,
+                    (index) => Container(
+                      margin: EdgeInsets.only(top: 20),
+                      height: 100,
+                      width: double.infinity,
+                      color: Colors.grey,
+                      child: Center(child: Text("Postingan Ke-$index")),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 15,
+            right: 12,
+            child: FloatingActionButton(
+              onPressed: () {},
+              shape: CircleBorder(),
+              backgroundColor: Colors.black,
+              child: Icon(Icons.add, color: Colors.white),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
